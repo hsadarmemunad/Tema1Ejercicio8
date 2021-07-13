369d3c704569ee47a5347308cffa2c704652181c
Henry Adarme <hsadarmem@unadvirtual.edu.co>
2021-07-13 10:36:07 -0500

    Agregar archivos de proyecto.

---
+++ Tema1Ejercicio8/Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxAltitude = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.LabelSpeed = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TextBoxAltitude)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(776, 160)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Lanzamiento vertical"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(383, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(33, 29)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "m"
+        '
+        'TextBoxAltitude
+        '
+        Me.TextBoxAltitude.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxAltitude.Location = New System.Drawing.Point(277, 80)
+        Me.TextBoxAltitude.Name = "TextBoxAltitude"
+        Me.TextBoxAltitude.Size = New System.Drawing.Size(100, 34)
+        Me.TextBoxAltitude.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(295, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Digite la altura en metros que desea alcanzar"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 225)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(290, 17)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "La piedra se debe lanzar a una velocidad de"
+        '
+        'LabelSpeed
+        '
+        Me.LabelSpeed.AutoSize = True
+        Me.LabelSpeed.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelSpeed.Location = New System.Drawing.Point(326, 286)
+        Me.LabelSpeed.Name = "LabelSpeed"
+        Me.LabelSpeed.Size = New System.Drawing.Size(71, 29)
+        Me.LabelSpeed.TabIndex = 4
+        Me.LabelSpeed.Text = "0 m/s"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 347)
+        Me.Controls.Add(Me.LabelSpeed)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Lanzamiento vertical"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxAltitude As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents LabelSpeed As Label
+End Class
---
+++ Tema1Ejercicio8/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub TextBoxAltitude_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxAltitude.KeyPress
+        OnlyNumbers(e)
+    End Sub
+
+    Private Sub TextBoxAltitude_TextChanged(sender As Object, e As EventArgs) Handles TextBoxAltitude.TextChanged
+        Dim altitude As Decimal
+        Dim speed As Decimal
+        Dim strAltitude As String
+
+        strAltitude = TextBoxAltitude.Text.Replace(".", ",")
+
+        If Decimal.TryParse(strAltitude, altitude) Then
+            speed = Math.Sqrt(2 * 9.8 * altitude)
+            LabelSpeed.Text = Format(speed, "0.00")
+        End If
+
+    End Sub
+
+    Private Sub OnlyNumbers(e As KeyPressEventArgs)
+        '48 - 57  = Ascii codes for numbers
+
+        If Asc(e.KeyChar) <> 8 Then
+            If Not (Char.IsDigit(e.KeyChar) Or e.KeyChar = ".") Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+End Class
